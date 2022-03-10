2fd539cbfaf823782efdb95d2ab9c680fd07e181
Yulin <ytsou1230@gmail.com>
2022-03-10 23:54:20 +1300

    Updates

---
+++ precache-manifest.ad7fffa06b9915da948461d75f48cc1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9056cf7b4bfeefef123c",
+    "url": "/robofriends/static/css/main.106e9021.chunk.css"
+  },
+  {
+    "revision": "9056cf7b4bfeefef123c",
+    "url": "/robofriends/static/js/main.669f3f67.chunk.js"
+  },
+  {
+    "revision": "04ba49df8c75a51ba00d",
+    "url": "/robofriends/static/js/runtime~main.8bd9438d.js"
+  },
+  {
+    "revision": "1d37400db0d647c8e42c",
+    "url": "/robofriends/static/css/2.c3620714.chunk.css"
+  },
+  {
+    "revision": "1d37400db0d647c8e42c",
+    "url": "/robofriends/static/js/2.a55fe2cd.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/robofriends/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "9fa430eb25ff33656f6e2b3d6ffa4e63",
+    "url": "/robofriends/index.html"
+  }
+];
